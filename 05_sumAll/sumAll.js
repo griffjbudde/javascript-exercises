f3b2fbc2c2ef46fe44cffedc3dfb96ba42192088
griffjbudde <griffonjbudde@gmail.com>
2025-03-24 17:34:39 -0600

    Solve excerise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(bottom, top,) {
+    let high = 0;
+    let low = 0;
+    if (bottom > top) {
+        high = bottom;
+        low = top;
+        }
+    else {
+        high = top;
+        low = bottom;
+    }
+    if (Number.isInteger(bottom + top) && bottom >= 0 && top  >= 0) {
+        let i = low;
+        let sum = 0;
+        while (i <= high) {
+            sum = sum + i;
+            i++;
+        }
+        return sum;
+    }
+    else {
+        return "ERROR"
+    }
+}
+console.log(sumAll(1, 5))
 // Do not edit below this line
 module.exports = sumAll;
