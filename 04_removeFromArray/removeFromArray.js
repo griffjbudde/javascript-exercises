ddbf87994d644a16f85fa7d3fd6260c708b6367d
griffjbudde <griffonjbudde@gmail.com>
2025-03-23 18:26:18 -0600

    Solve excercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-};
+let theArray = []
 
+const removeFromArray = function(theArray, ...argument) {
+   return theArray.filter(val => !argument.includes(val));
+}
+console.log(removeFromArray([1, 2, 3, 4], 3, 2))
 // Do not edit below this line
 module.exports = removeFromArray;
