9c2ea70e1e0819316f0ce5f2f2df6c8148408afd
griffjbudde <griffonjbudde@gmail.com>
2025-03-24 18:06:48 -0600

    Solve excercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  cels = (far - 32) * (5 / 9);
+  roundCels = Math.round(cels * 10) / 10;
+  return roundCels;
 };
 
-const convertToFahrenheit = function() {
-};
+
+const convertToFahrenheit = function(cels) {
+  far = (cels * (9 / 5) + 32);
+  roundFar = Math.round(far * 10) / 10;
+  return roundFar;
+}
 
 // Do not edit below this line
 module.exports = {
